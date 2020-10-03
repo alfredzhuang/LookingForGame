6217ea81c966fb6807058a46a54ea15bd0ae95a9
Andrei <andreiidimaano@gmail.com>
2020-10-02 18:53:18 -0700

    add homepage

--- src/components/pages/Homepage.js
+++ src/components/pages/Homepage.js
@@ -7,7 +7,47 @@ function homepage() {
         <div className="homepage">
             <div className="section">
                 <NavbarUser />
-                <h2>homepage</h2>
+                <div className="homepage-inner">
+                    <h2>Your Groups</h2>
+                    <div className="homepage-content">
+                        <div className="flex-container">
+                            <div className="flex-fill">
+                                <img className="flex-img" src="/images/spiritblossom.jpg" alt="hello"/>
+                            </div>
+                            <h3>League of Legends</h3>
+                        </div>
+                        <div className="flex-container">
+                            <div className="flex-fill">
+                                <img className="flex-img" src="/images/spiritblossom.jpg" alt="hello"/>
+                            </div>
+                            <h3>League of Legends</h3>
+                        </div>
+                        <div className="flex-container">
+                            <div className="flex-fill">
+                                <img className="flex-img" src="/images/spiritblossom.jpg" alt="hello"/>
+                            </div>
+                            <h3>League of Legends</h3>
+                        </div>
+                        <div className="flex-container">
+                            <div className="flex-fill">
+                                <img className="flex-img" src="/images/spiritblossom.jpg" alt="hello"/>
+                            </div>
+                            <h3>League of Legends</h3>
+                        </div>
+                        <div className="flex-container">
+                            <div className="flex-fill">
+                                <img className="flex-img" src="/images/spiritblossom.jpg" alt="hello"/>
+                            </div>
+                            <h3>League of Legends</h3>
+                        </div>
+                        <div className="flex-container">
+                            <div className="flex-fill">
+                                <img className="flex-img" src="/images/spiritblossom.jpg" alt="hello"/>
+                            </div>
+                            <h3>League of Legends</h3>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
