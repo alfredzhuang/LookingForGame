b63d2eb35532931bbea2676cc77486286e8dcb14
Andrei <andreiidimaano@gmail.com>
2020-10-03 15:58:05 -0700

    fix css

--- src/components/pages/Create.js
+++ src/components/pages/Create.js
@@ -43,38 +43,32 @@ let create = (props) => {
                         <h2>Create a Group</h2>
                     </div>
                     <div className="create-form">
-                        <div>
-                            <div className="create-input-container group-name">
-                                <label 
-                                    className="create-label" 
-                                    htmlFor="create-group-name">Group Name</label>
-                                <input 
-                                    placeholder="Enter your Group Name" 
-                                    id= "create-group-name" 
-                                    className="input" 
-                                    type="text" 
-                                    required 
-                                    value={name} 
-                                    onChange={(e) => setName(e.target.value)}></input>
-                            </div>
-                        </div> 
-                        <div>
-                            <div className="create-input-container choose-game">
-                                <label htmlFor="game">Choose a Game:</label>
-                                    <select name="game" id="game" value={game} onChange={handleChange}>
-                                        <option value="LeagueofLegends" selected="selected">League of Legends</option>
-                                        <option value="AmongUs">Among Us</option>
-                                        <option value="Minecraft">Minecraft</option>
-                                        <option value="Genshin">Genshin Impact</option>
-                                        <option value="Smash">Smash</option>
-                                        <option value="Valorant">Valorant</option>
-                                        <option value="Phasmophobia">Phasmophobia</option>
-                                    </select>
-                            </div>
+                        <div className="create-input-container group-name">
+                            <label 
+                                className="create-label" 
+                                htmlFor="create-group-name">Group Name</label>
+                            <input 
+                                placeholder="Enter your Group Name" 
+                                id= "create-group-name" 
+                                className="input" 
+                                type="text" 
+                                required 
+                                value={name} 
+                                onChange={(e) => setName(e.target.value)}></input>
                         </div>
-                     </div>
-                     <div>
-                         <div className="create-input-container discord-link">
+                        <div className="create-input-container choose-game">
+                            <label htmlFor="game">Choose a Game:</label>
+                            <select name="game" id="game" value={game} onChange={handleChange}>
+                                <option value="LeagueofLegends" selected="selected">League of Legends</option>
+                                <option value="AmongUs">Among Us</option>
+                                <option value="Minecraft">Minecraft</option>
+                                <option value="Genshin">Genshin Impact</option>
+                                <option value="Smash">Smash</option>
+                                <option value="Valorant">Valorant</option>
+                                <option value="Phasmophobia">Phasmophobia</option>
+                            </select>
+                        </div>
+                        <div className="create-input-container discord-link">
                              <label 
 								className="create-label"
 						        htmlFor="create-discord-link">Messaging Invite Link</label>
@@ -87,30 +81,31 @@ let create = (props) => {
                                 value={discord} 
                                 onChange={(e) => setDiscord(e.target.value)}></input>
                          </div>
-                     </div>
-                         <div className="create-last">
-                             <div className="create-input-container Description">
-                                 <label 
-								    className="create-label" 
-									htmlFor="create-Description">Description</label>
-                                 <input 
-									placeholder="Enter your Group Description" 
+                        <div>
+                            <div className="create-input-container Description">
+                                <label 
+                                    className="create-label" 
+                                    htmlFor="create-Description">Description</label>
+                                <input 
+								    placeholder="Enter your Group Description" 
 									id= "create-Description" 
 									className="input" 
 									type="text" 
 								    required 
                                     value={description} 
                                     onChange={(e) => setDescription(e.target.value)}></input>
-                             </div>
-                         </div>
-                        <div>
-                            <input type = "file" onChange = {changeFile}/>
+                            </div>
                         </div>
-                     <div>
-                         <button onClick={handleGroup} className="create-button">
-                             Create Group
-                         </button>
-                     </div>
+                        <div className="create-input-container create-last">
+                            <label className="create-label" htmlFor="group-img">Upload a Group Image</label>
+                            <input id="group-img" type="file" onChange={changeFile}/>
+                        </div>
+                        <div>
+                            <button onClick={handleGroup} className="create-button">
+                                Create Group
+                            </button>
+                        </div>
+                    </div>
                  </div>
              </div>
          </div>
